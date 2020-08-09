6c2c471b56b3b95e5773738d796af6ff64d1986b
youngwoo.noh <youngwoo.noh@bespinglobal.com>
2020-08-09 18:35:40 +0900

    "HelloController" 테스트 코드 추가

---
+++ src/main/java/org/ppoocoder/springboot/web/HelloController.java
@@ -0,0 +1,16 @@
+package org.ppoocoder.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//RestController : Json을 반환하는 컨트롤러로 만들어줌
+//                 예전에 @ResponseBody를 각 메소드마다 선언했던것을 한번에 사용할수 있게 해줌
+@RestController
+public class HelloController {
+    // HTTP Method인 Get요청을 API로 만들어줌
+    //      예전에 @RequestMapping(method=RequetMethd.GET)으로 사용됨
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
